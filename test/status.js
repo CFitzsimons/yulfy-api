67e243dd9bf79f5b3ef8ff58c0039dea6eb10e3a
CFitzsimons <yulfy@outlook.com>
2017-07-24 18:09:35 +0100

    Updated tests

--- test/status.js
+++ test/status.js
@@ -5,7 +5,14 @@ describe('Status', () => {
   describe('#website()', () => {
     it('should be online', (done) => {
       const site = 'https://google.com';
-      website.isUp(site).then(() => done());
+      website.isUp(site).then((res) => {
+        console.log(res);
+        if (res.status && res.time) {
+          done();
+        } else {
+          done('http://google.com returned an error');
+        }
+      });
     });
   });
 });
