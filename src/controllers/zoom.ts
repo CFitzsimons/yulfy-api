aa0379082d5b546539641128d8f33afd1af58bb7
Colin Fitzsimons <colin@cleancodecompany.com>
2024-03-28 15:36:39 +0000

    feat: log response message

--- src/controllers/zoom.ts
+++ src/controllers/zoom.ts
@@ -1,7 +1,8 @@
 import Express from 'express';
 
 const listen = (req: Express.Request, res: Express.Response) => {
-
+  console.log(req.body);
+  res.send('Ok');
 };
 
 const get = (req: Express.Request, res: Express.Response) => {
